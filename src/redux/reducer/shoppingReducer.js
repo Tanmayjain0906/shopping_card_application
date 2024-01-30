7eadbe5c10094dc7eb2d36e2ff28dbc60f0e9e33
Tanmayjain0906 <tanmayjain983@gmail.com>
2024-01-30 16:48:53 +0530

    cart

--- src/redux/reducer/shoppingReducer.js
+++ src/redux/reducer/shoppingReducer.js
@@ -4,12 +4,37 @@ const initialValue = [];
 
 const shoppingReducer = (state=initialValue, action) => {
 
-    switch(action.type)
+    // switch(action.type)
+    // {
+    //     case ADD_TO_CART: return [...state, action.payload];
+    //     case REMOVE_FROM_CART: return state.filter(product => product.id != action.payload)
+    //     case CHECKOUT: return [];
+    //     default: return state;
+    // }
+
+    if(action.type == ADD_TO_CART)
     {
-        case ADD_TO_CART: return [...state, action.payload];
-        case REMOVE_FROM_CART: return state.filter(product => product.id != action.payload)
-        case CHECKOUT: return [];
-        default: return state;
+       const arr = [...state];
+       for(let i=0;i<arr.length;i++)
+       {
+        if(arr[i].title == action.payload.title)
+        {
+            return [...state];
+        }
+       }
+       return [...state, action.payload];
+    }
+    else if(action.type == REMOVE_FROM_CART)
+    {
+        return state.filter(product => product.id != action.payload);
+    }
+    else if(action.type == CHECKOUT)
+    {
+        return [];
+    }
+    else
+    {
+        return state;
     }
 }
 
